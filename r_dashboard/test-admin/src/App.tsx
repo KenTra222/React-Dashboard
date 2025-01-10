b2bfb4f2cccedfaef90226789acf04e9677e79e5
KenTra222 <Kentraviousc@gmail.com>
2025-01-10 07:01:50 +0000

    updated post show and user files

--- r_dashboard/test-admin/src/App.tsx
+++ r_dashboard/test-admin/src/App.tsx
@@ -1,20 +1,26 @@
 import {
   Admin,
   Resource,
+  ShowGuesser,
+  // ListGuesser
   // EditGuesser,
   // ShowGuesser,
 } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import PostList from "./pages/posts/post-list";
+import { UserList } from "./pages/users/user-list";
+import { PostShow } from "./pages/posts/post-show";
+import { UserShow } from "./pages/users/user-show";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} show={PostShow}/>
+    <Resource name="users" list={UserList} show={UserShow}/>
     {/* <Resource name="comments" list={ListGuesser}/>
     <Resource name="albums" list={ListGuesser} />
     <Resource name="photos" list={ListGuesser}/>
     <Resource name="todos" list={ListGuesser} />
-    <Resource name="users" list={ListGuesser}/> */}
+     */}
   </Admin>
 );
